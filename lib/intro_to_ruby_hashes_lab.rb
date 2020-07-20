4c3b081190455f58fc5b58bc3999e56525ec602d
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-20 17:00:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ my_hash_creator
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
